637cbaf33c7e2ca1c7fba3f781d64cea606c8de8
Jens Klaren <jensklaren@gmail.com>
2018-11-01 20:01:26 +1100

    Added Model BookRoom

---
+++ GroupProjectWebHotel/Models/BookRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProjectWebHotel.Models
+{
+    public class BookRoom
+    {
+        [Key, Required]
+        [Range(1, 16)]
+        public int RoomID { get; set; }
+        public string CustomerEmail { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckIn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckOut { get; set; }
+
+        public Room TheRoom { get; set; }
+        public Customer TheCustomer { get; set; }
+    }
+}
