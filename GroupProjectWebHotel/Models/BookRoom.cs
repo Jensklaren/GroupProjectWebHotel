3bb39eb9e4450166daa11da46ef031019a6858eb
cobrien1 <cobrien1@LAPTOP-ET95LRJ5>
2018-11-01 20:22:49 +1100

    Book a Room stuff updated

--- GroupProjectWebHotel/Models/BookRoom.cs
+++ GroupProjectWebHotel/Models/BookRoom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace GroupProjectWebHotel.Models
 {
     public class BookRoom
     {
+        public int RoomID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CheckIn { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CheckOut { get; set; }
     }
 }
